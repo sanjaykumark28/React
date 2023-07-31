84cc819530e3ce3fbd22c84d819caacba02b6616
SANJAY KUMAR K <138999591+sanjaykumark28@users.noreply.github.com>
2023-07-31 16:47:59 +0530

    Add files via upload

---
+++ bulb/script.js
@@ -0,0 +1,6 @@
+function off(){
+                document.getElementById('img').src='incandescent-bulb@2x.jpg'
+            }
+            function on(){
+                document.getElementById('img').src='48123160-bright-glowing-incandescent-light-bulb-on-a-white-background.webp'
+            }   
